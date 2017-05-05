798e635243f58e5330341ea5c910e77a8c2a1378
c74d <8573dd@gmail.com>
2017-05-05 21:59:03 +0000

    Replace hand-rolled logger with env_logger

--- src/bin/bot74d.rs
+++ src/bin/bot74d.rs
@@ -1,12 +1,11 @@
 extern crate clap;
+extern crate env_logger;
 extern crate irc_bot;
 
 #[macro_use]
 extern crate log;
 
 use irc_bot::modules;
-use std::io;
-use std::io::Write as IoWrite;
 
 const PROGRAM_NAME: &'static str = "bot74d";
 
@@ -17,13 +16,7 @@ fn main() {
                  .default_value("config.json"))
         .get_matches();
 
-    let log_lvl = log::LogLevelFilter::Info;
-
-    log::set_logger(|max_log_lvl| {
-                        max_log_lvl.set(log_lvl);
-                        Box::new(LogBackend { log_lvl: log_lvl })
-                    })
-            .expect("error: failed to initialize logging");
+    env_logger::init().expect("error: failed to initialize logging");
 
     irc_bot::run(args.value_of("config-file").expect("default missing?"),
                  |err| {
@@ -31,22 +24,4 @@ fn main() {
                      irc_bot::ErrorReaction::Proceed
                  },
                  &[modules::default(), modules::test()]);
-}
-
-
-struct LogBackend {
-    log_lvl: log::LogLevelFilter,
-}
-
-impl log::Log for LogBackend {
-    fn enabled(&self, metadata: &log::LogMetadata) -> bool {
-        metadata.level() <= self.log_lvl
-    }
-
-    fn log(&self, record: &log::LogRecord) {
-        if !self.enabled(record.metadata()) {
-            return;
-        }
-        writeln!(io::stderr(), "{}: {}", record.level(), record.args()).expect("stderr broken?");
-    }
 }
