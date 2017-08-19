e794e42292be21b209fee17151a4d0693c45ef1c
c74d <8573dd@gmail.com>
2017-08-19 04:35:19 +0000

    shell.nix: Upgrade to latest stable Rust

--- shell.nix
+++ shell.nix
@@ -1,21 +1,28 @@
 let
-  inherit (import <nixpkgs> {})
-    lib
-    stdenv
-    cargo
-    clang
-    rustc
-    rustfmt
-  ;
+  nixpkgs-mozilla = (import <nixpkgs> {}).fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    rev = "ca0031baaac0538b9089625c8fa0b790b4270d36";
+    sha256 = "0albnrwnx5ixgxvlrrcdyjsh5r25bqiw0xw7kdgi298inwyz3xz5";
+  };
+
+  rust-overlay = "${nixpkgs-mozilla}/rust-overlay.nix";
 in
+
+with import <nixpkgs> {
+  overlays = [
+    (import rust-overlay)
+  ];
+};
 
 stdenv.mkDerivation rec {
   name = "irc-bot.rs";
 
   nativeBuildInputs = [
-    cargo
+    (rustChannelOf {
+      channel = "1.19.0";
+    }).rust
     clang
-    rustc
     rustfmt
   ];
 
