644feab448a8c003298f9d5b253313f8c7ff69e3
c74d <8573dd@gmail.com>
2018-01-04 04:07:20 +0000

    egbot: Add option for error verbosity

--- src/bin/egbot.rs
+++ src/bin/egbot.rs
@@ -1,6 +1,8 @@
-extern crate clap;
 extern crate env_logger;
 extern crate irc_bot;
+
+#[macro_use]
+extern crate clap;
 
 #[macro_use]
 extern crate log;
@@ -14,16 +16,37 @@ fn main() {
                 .short("c")
                 .default_value("config.yaml"),
         )
+        .arg(
+            clap::Arg::with_name("error-verbosity")
+                .long("error-verbosity")
+                .possible_values(&ErrorVerbosity::variants())
+                .case_insensitive(true)
+                .default_value("Display"),
+        )
         .get_matches();
 
     env_logger::init().expect("error: failed to initialize logging");
 
+    let error_verbosity = value_t!(args, "error-verbosity", ErrorVerbosity)
+        .unwrap_or_else(|err| err.exit());
+
     irc_bot::run(
         irc_bot::Config::try_from_path(args.value_of("config-file").expect("default missing?")),
-        |err| {
-            error!("{:?}", err);
+        move |err| {
+            match error_verbosity {
+                ErrorVerbosity::Display => error!("{}", err),
+                ErrorVerbosity::Debug => error!("{:?}", err),
+            }
             irc_bot::ErrorReaction::Proceed
         },
         &[modules::default, modules::test],
     );
+}
+
+arg_enum! {
+    #[derive(Debug)]
+    enum ErrorVerbosity {
+        Display,
+        Debug
+    }
 }
