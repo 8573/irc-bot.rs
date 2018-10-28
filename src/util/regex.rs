b51fc83f641eee1c5fbbf1166faff628aafa394e
c74d <8573dd@gmail.com>
2018-10-28 06:06:03 +0000

    Double generic Regex size limits

--- src/util/regex.rs
+++ src/util/regex.rs
@@ -9,9 +9,8 @@ type RegexBuildResult = std::result::Result<Regex, regex::Error>;
 fn mk_case_insensitive_regex(s: &str) -> RegexBuildResult {
     let mut rx = RegexBuilder::new(s);
     rx.case_insensitive(true);
-    // TODO: Increase these limits
-    rx.size_limit(1 << 16);
-    rx.dfa_size_limit(1 << 16);
+    rx.size_limit(1 << 17);
+    rx.dfa_size_limit(1 << 17);
     rx.build()
 }
 
