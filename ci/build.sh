d952fc4a32f66c0ce7249d4c058e29ec4e72e268
c74d <8573dd@gmail.com>
2020-03-20 01:57:41 +0000

    CI: Print version info in before_script not script
    In CI, print version information about Rust and Cargo in the
    `before_script` scripts rather than in the `script` scripts.

--- ci/build.sh
+++ ci/build.sh
@@ -6,11 +6,6 @@ set -euv
 find . \( -iname '*.sh' -o -iname '*.bash' -o -iname '*.zsh' \) -print0 |
     xargs -0t shellcheck --enable=all --check-sourced --external-sources
 
-# Print our Rust and Cargo versions in case that's useful for debugging.
-echo "${TRAVIS_RUST_VERSION:-n/a}"
-rustc --version
-cargo --version
-
 # Check that all our source-code formatting is standard.
 cargo fmt --all -- --check
 
--- ci/pre-build.sh
+++ ci/pre-build.sh
@@ -2,6 +2,12 @@
 
 set -euv
 
+# Print our Rust and Cargo versions in case that's useful for debugging.
+echo "${TRAVIS_RUST_VERSION:-n/a}"
+rustc --version
+cargo --version
+
+# Install Rust-related tools.
 rustup component add rustfmt
 cargo install --force cargo-audit
 cargo install --force cargo-tree
